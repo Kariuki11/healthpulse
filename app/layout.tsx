875bbe1f7ed1bb2a3b854ae612645660000d3748
Kariuki 11 <irungukenneth433@gmail.com>
2024-12-16 12:28:12 +0300

    Enhance styling and layout for HealthPulse application; add utility functions and update Tailwind configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from '@/lib/utils'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "HealthPulse",
-  description: "HealthPulse is a global health Management System.",
+  description: "HealthPulse is a global health Management System",
 };
 
 export default function RootLayout({
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn('min-h-screen bg-dark-300 font-sans antialised', fontSans.variable)}
       >
         {children}
       </body>
